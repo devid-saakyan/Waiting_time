47e165a1cc02da291929209d5220d2a769d05c76
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2021-03-24 22:00:52 +0300

    Add files via upload

---
+++ praktika.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+c = Canvas(root, width = 600, height = 600, bg = '#27E6EC')
+c.pack()
+
+entry1 = Entry(root)
+c.create_window(200, 140, window=entry1, height = 20, width = 30)
+
+label2 = Label(root, text='Введите год:')
+label2.config(font=('helvetica', 10))
+c.create_window(140, 140, window=label2)
+
+root.mainloop()
